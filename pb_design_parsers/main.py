37f6175d6eb59fd7617a24f7edcc695d1fc4f734
Vcslav <vcslav.v@gmail.com>
2021-10-27 16:28:04 +0300

    fix

--- pb_design_parsers/main.py
+++ pb_design_parsers/main.py
@@ -18,7 +18,7 @@ def send_tg_alarm(message):
             ))
 
 
-@sched.scheduled_job('cron', hour=13, minute=25)
+@sched.scheduled_job('cron', hour=1)
 @logger.catch
 def parse_creative_market():
     logger.info('Start parsing')
