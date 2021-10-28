6fcbbe5a26c074a5f6975eaf7cb66de85cdd06d4
Vcslav <vcslav.v@gmail.com>
2021-10-28 20:02:07 +0300

    test

--- pb_design_parsers/main.py
+++ pb_design_parsers/main.py
@@ -21,12 +21,18 @@ def send_tg_alarm(message):
 @sched.scheduled_job('cron', hour=1)
 @logger.catch
 def parse_creative_market():
-    logger.info('Start parsing')
+    logger.info('Start parsing cm')
     creative.parse(os.environ.get('CM_USER'), os.environ.get('CM_USER_PASS'))
     creative.parse(os.environ.get('CM_USER_1'), os.environ.get('CM_USER_PASS_1'))
 
 
+# @sched.scheduled_job('cron', hour=2)
+# @logger.catch
+# def parse_elements():
+#     logger.info('Start parsing elements')
+#     envanto.parse(os.environ.get('ELEM_USER'), os.environ.get('ELEM_USER_PASS'))
+
+
 if __name__ == "__main__":
     logger.add(sink=send_tg_alarm)
-    envanto.parse(os.environ.get('ELEM_USER'), os.environ.get('ELEM_USER_PASS'))
     sched.start()
