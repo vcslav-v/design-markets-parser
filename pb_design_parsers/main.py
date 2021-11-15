b969a117f5118193fb96dadb63034730fb8c08b5
Vcslav <vcslav.v@gmail.com>
2021-11-15 14:03:51 +0300

    test

--- pb_design_parsers/main.py
+++ pb_design_parsers/main.py
@@ -37,7 +37,7 @@ def parse_items():
     envanto.refresh_products(os.environ.get('ELEM_USER'), os.environ.get('ELEM_USER_PASS'))
 
 
-@sched.scheduled_job('cron', hour=10, minute=47)
+@sched.scheduled_job('cron', hour=11, minute=6)
 @logger.catch
 def parse_elements_items():
     logger.info('TEST')
